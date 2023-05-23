1898ec565443a2d152c27171f68250f95f2fb37c
mathews-hos pc paula <mathewshosoares@gmail.com>
2023-05-23 00:32:00 -0300

    quase terminandin

---
+++ js/script.js
@@ -0,0 +1,30 @@
+function areacirc() {
+    let inp = document.getElementById("input-raio-circ")
+    let out = document.getElementById("output-raio-circ")
+    out.value = (parseFloat(inp.value)*parseFloat(inp.value)) * 3.14
+}
+
+function hipotenusa() {
+
+
+}
+
+function realdoll() {
+
+
+}
+
+function celsofarenrait() {
+
+
+}
+
+function medianota() {
+
+
+}
+
+function maskara() {
+
+
+}
